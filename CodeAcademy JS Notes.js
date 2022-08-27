dc1e681332797a0078fb06667fac7d4560e4b2eb
Nurmyrat AMANMADOV <amanmadov@gmail.com>
2022-08-27 12:04:23 -0700

    Update CodeAcademy JS Notes.js

--- CodeAcademy JS Notes.js
+++ CodeAcademy JS Notes.js
@@ -2174,50 +2174,50 @@ Extra Link: https://reactkungfu.com/2015/10/the-difference-between-virtual-dom-a
 
 //#region app.js 
 
-    import { animals } from "./animals";
-    import React from "react";
-    import ReactDOM from "react-dom";
+import { animals } from "./animals";
+import React from "react";
+import ReactDOM from "react-dom";
 
-    const title = "";
-    const background = (
+const title = "";
+const background = (
     <img className="background" alt="ocean" src="/images/ocean.jpg" />
-    );
+);
 
 
-    const images = [];
-    for (const animal in animals) {
+const images = [];
+for (const animal in animals) {
     images.push(<img
         key={animal}
         className='animal'
         alt={animal}
         src={animals[animal].image}
         aria-label={animal}
-        role='button' 
-        onClick={displayFact} 
-        />)
-    };
+        role='button'
+        onClick={displayFact}
+    />)
+};
 
-    function displayFact(e) {
+function displayFact(e) {
     const facts = animals[e.target.alt].facts;
     const randomFactIndex = Math.floor(Math.random() * facts.length)
     const fact = facts[randomFactIndex];
     document.getElementById('fact').innerHTML = fact;
-    }
+}
 
-    const animalFacts = (
+const animalFacts = (
     <div>
-        <h1>{ title === '' ? 'Click an animal for a fun fact' : title }</h1>
-        { background}
-        <img 
-        className='background'
-        alt='ocean'
-        src='/images/ocean.jpg' />
+        <h1>{title === '' ? 'Click an animal for a fun fact' : title}</h1>
+        {background}
+        <img
+            className='background'
+            alt='ocean'
+            src='/images/ocean.jpg' />
         <div className='animals'>{images}</div>
         <p id='fact'></p>
     </div>
-    );
+);
 
-    ReactDOM.render(animalFacts, document.getElementById("root"));
+ReactDOM.render(animalFacts, document.getElementById("root"));
 
 //#endregion
 
@@ -2259,6 +2259,11 @@ Extra Link: https://reactkungfu.com/2015/10/the-difference-between-virtual-dom-a
 
 //#endregion
 
+//#region Authorization Form 
+
+
+
+//#endregion
 
 
 

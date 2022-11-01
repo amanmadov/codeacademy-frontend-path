c0fec6b08110145015f2c3bd5a48dd03f7131558
Nurmyrat AMANMADOV <amanmadov@gmail.com>
2022-11-01 13:58:01 -0700

    completed vanilla js section
    Vanilla.js sections completed. Starting for React

--- CodeAcademy JS Notes.js
+++ CodeAcademy JS Notes.js
@@ -3047,22 +3047,135 @@ const url = "https://api.datamuse.com/words?sl=";
 
 //#region Intro to async GET Requests 
 
+// In the following exercises, we’re going to take what we’ve learned about chaining Promises and make it simpler using functionality introduced in ES8: async and await. 
+// You read that right, you did the hard part already. 
+// Now it’s time to make it easier.
+
+// The structure for this request will be slightly different. We will use the new keywords async and await, as well as the try and catch statements.
+
+// Here are some key points to keep in mind as we walk through the code:
+    // The async keyword is used to declare an async function that returns a promise.
+    // The await keyword can only be used within an async function. await suspends the program while waiting for a promise to resolve.
+    // In a try...catch statement, code in the try block will be run and in the event of an exception, the code in the catch statement will run.
+
+// Study the async getData() function to the right to see how the request can be written using async and await.
+
+//#endregion
+
+//#region Making an async GET Request 
+
+// In this exercise, we’re going to apply the code to get nouns that describe the inputted word using the Datamuse API.
+
+// Information to reach API
+    const url = 'https://api.datamuse.com/words?';
+    const queryParams = 'rel_jja=';
+
+// Selecting page elements
+    const inputField = document.querySelector('#input');
+    const submit = document.querySelector('#submit');
+    const responseField = document.querySelector('#responseField');
+
+// Asynchronous function
+    const getSuggestions = async () => {
+        const wordQuery = inputField.value;
+        const endpoint = url + queryParams + wordQuery;
+        try {
+            const response = await fetch(endpoint, { cache: 'no-cache' });
+            if (response.ok) {
+                const jsonResponse = await response.json();
+                renderResponse(jsonResponse);
+            }
+        } catch (e.message) {
+            console.log(e);
+        }
+    };
+
+// Clear previous results and display results to webpage
+    const displaySuggestions = (event) => {
+        event.preventDefault();
+        while (responseField.firstChild) {
+            responseField.removeChild(responseField.firstChild)
+        }
+        getSuggestions();
+    }
+
+    submit.addEventListener('click', displaySuggestions);
+
+//#endregion
+
+//#region Intro to async POST Requests 
+
+// Now that you’ve made an async GET request, let’s start getting familiar with the async POST request.
+// As we’ve seen before, a POST request requires more information. 
+
+// We still have the same structure of using try and catch as the async GET request we just learned about. 
+// But, in the fetch() call, we now have to include an additional argument that contains more information like method and body.
+
+// The method property value is set to 'POST' to specify the type of request we are making. 
+// Then we have to include a body property with the value of JSON.stringify({id: 200}).
+
+//#endregion
+
+//#region Making an async POST Request 
+
+// Since we’ve reviewed the boilerplate code for an async POST request, 
+// the next step is to incorporate that logic into making a real request.
+// In this exercise, we’ll need to retrieve our Rebrandly API key to access the Rebrandly API.
+// We will then pass in the endpoint and the request object into the fetch() method to make our POST request.
+// If you reset the exercise at any point, you will have to paste in your API key again at the top!
+
+// information to reach API
+    const apiKey = '<Your API Key>';
+    const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+    const inputField = document.querySelector('#input');
+    const shortenButton = document.querySelector('#shorten');
+    const responseField = document.querySelector('#responseField');
+
+// Asynchronous functions
+    const shortenUrl = async () => {
+        const urlToShorten = inputField.value;
+        const data = JSON.stringify({ destination: urlToShorten });
+        try {
+            const response = await fetch(
+                url,
+                {
+                    method: 'POST',
+                    body: data,
+                    headers: {
+                        'Content-type': 'application/json',
+                        'apikey': apiKey
+                    }
+                }
+            );
+            if (response.ok) {
+                const jsonResponse = await response.json();
+                renderResponse(jsonResponse);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+// Clear page and call Asynchronous functions
+    const displayShortUrl = (event) => {
+        event.preventDefault();
+        while (responseField.firstChild) {
+            responseField.removeChild(responseField.firstChild);
+        }
+        shortenUrl();
+    }
+
+    shortenButton.addEventListener('click', displayShortUrl);
+
+//#endregion
 
 
 //#endregion
 
 
-
-
-
 //#endregion
-
-
-//#endregion
-
-
-
-
 
 
 //#endregion

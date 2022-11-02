d6cf26f1ddbf4e6529793f9b738cb5c0419a8d4f
Nurmyrat AMANMADOV <amanmadov@gmail.com>
2022-11-02 00:20:54 -0700

    Update CodeAcademy JS Notes.js

--- CodeAcademy JS Notes.js
+++ CodeAcademy JS Notes.js
@@ -4472,7 +4472,53 @@ Extra Link: https://reactkungfu.com/2015/10/the-difference-between-virtual-dom-a
 
     //#endregion
 
+    //#region Pass props From Component To Component 
 
+        // The most common use of props is to pass information to a component, from a different component. 
+        // You haven’t done that yet, but it’s very similar to what you’ve seen already.
+
+        // A curmudgeonly clarification about grammar:
+        // You may have noticed some loose usage of the words prop and props. 
+        // Unfortunately, this is pretty inevitable.
+
+        // props is the name of the object that stores passed-in information. 
+        // this.props refers to that storage object. 
+        // At the same time, each piece of passed-in information is called a prop. 
+        // This means that props could refer to two pieces of passed-in information, 
+        // or it could refer to the object that stores those pieces of information :(
+
+
+        // Greeting.js
+        import React from 'react';
+        export class Greeting extends React.Component {
+            render() {
+                return <h1>Hi there, {this.props.name}!</h1>;
+            }
+        }
+
+        // App.js
+        import React from 'react';
+        import ReactDOM from 'react-dom';
+        import { Greeting } from './Greeting';
+
+        class App extends React.Component {
+            render() {
+                return (
+                    <div>
+                        <h1>Hullo and, "Welcome to The Newzz," "On Line!"</h1>
+                        <Greeting name='Nury' />
+                        <article>Latest newzz:  where is my phone?</article>
+                    </div>
+                );
+            }
+        }
+
+        ReactDOM.render(
+            <App />,
+            document.getElementById('app')
+        );
+
+    //#endregion
 
 
 //#endregion
